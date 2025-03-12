910492bb88e2c188410574781bb6b7cce2bbc6fc
mcodev <mcodevns@gmail.com>
2025-03-12 18:37:45 +0200

    Fix type

--- FE/web/types/common.ts
+++ FE/web/types/common.ts
@@ -4,7 +4,7 @@ import i18nConfig from '@/i18n/i18nConfig';
 export type LanguageType = (typeof i18nConfig.locales)[number];
 
 export type ParamsType = {
-  params: { locale: LanguageType };
+  params: Promise<{ locale: LanguageType }>;
 };
 
 export type NamespacesType = (typeof NAMESPACES_LIST)[number];
