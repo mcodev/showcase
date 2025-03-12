ae7e1da2eed606f10dfad56a251c84375f4abda0
mcodev <mcodevns@gmail.com>
2025-03-12 21:34:06 +0200

    update

--- FE/web/Providers/UserProvider.tsx
+++ FE/web/Providers/UserProvider.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 
 type UserProviderProps = {
   children: React.ReactNode;
 };
 
 const UserProvider = ({ children }: UserProviderProps) => {
-  useEffect(() => {
-    console.log('TestProvider');
-  }, []);
-
   return <div>{children}</div>;
 };
 
