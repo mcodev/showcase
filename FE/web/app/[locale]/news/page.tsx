16cab9c2a855291e7ed128c4c5b445a9866f6612
mcodev <mcodevns@gmail.com>
2025-03-13 09:46:04 +0200

    update

--- FE/web/app/[locale]/news/page.tsx
+++ FE/web/app/[locale]/news/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { NAMESPACE } from '@/i18n/consts';
+import initTranslations from '@/i18n/i18n';
+import { ParamsType } from '@/types/common';
 
-const News = () => {
-  return <div>News</div>;
+const News = async ({ params }: ParamsType) => {
+  const { locale } = await params;
+
+  // Server side translations
+  const { t } = await initTranslations(locale, NAMESPACE.HOME);
+
+  return <div>{t('news')}</div>;
 };
 
 export default News;
