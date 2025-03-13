9ff19a4ac27286cb8aa2fc972a7c60a574e939d9
mcodev <mcodevns@gmail.com>
2025-03-13 09:42:03 +0200

    update

--- FE/web/components/NavBar/NavBar.tsx
+++ FE/web/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import { Flex } from '@mantine/core';
 import { NAVBAR_ROUTES } from '@/common/consts';
@@ -16,17 +17,23 @@ const NavBar = ({ t, locale }: NavBarProps) => {
   return (
     <nav className={styles.wrapper} id="navbar">
       <div className="content_restriction">
-        <Link href="/" className={styles.logo}>
-          <p className={styles.site_name}>Riderz</p>
-        </Link>
+        <Flex align="center">
+          <Link href="/" className={styles.logo}>
+            <Image src="/logo.png" alt="logo" width={32} height={32} />
+
+            <p className={styles.site_name}>Riderz</p>
+          </Link>
+        </Flex>
 
         <Flex gap="var(--spacing-lg)" align="center">
           <nav className={styles.links_wrapper}>
-            {NAVBAR_ROUTES.map((route) => (
-              <Link href={`/${locale}${route.href}`} key={route.transKey} className={styles.link}>
-                {t(`layout:${route.transKey}`)}
-              </Link>
-            ))}
+            {NAVBAR_ROUTES.map((route) => {
+              return (
+                <Link href={`/${locale}${route.href}`} key={route.transKey} className={styles.link}>
+                  {t(`layout:${route.transKey}`)}
+                </Link>
+              );
+            })}
           </nav>
 
           <Profile />
