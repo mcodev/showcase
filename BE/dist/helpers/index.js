f547e7e0722906e995d5cf7f5823f675fc7c4939
mcodev <mcodevns@gmail.com>
2025-03-14 13:56:11 +0200

    update

--- BE/dist/helpers/index.js
+++ BE/dist/helpers/index.js
@@ -3,8 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.comparePassword = exports.hashPassword = void 0;
+exports.authentication = exports.random = exports.comparePassword = exports.hashPassword = void 0;
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const crypto_1 = __importDefault(require("crypto"));
+const SECRET = process.env.SECRET;
 const hashPassword = async (password) => {
     return await bcryptjs_1.default.hash(password, 10);
 };
@@ -13,4 +15,15 @@ const comparePassword = async (password, hash) => {
     return await bcryptjs_1.default.compare(password, hash);
 };
 exports.comparePassword = comparePassword;
+const random = () => {
+    return crypto_1.default.randomBytes(128).toString("base64");
+};
+exports.random = random;
+const authentication = (salt, password) => {
+    return crypto_1.default
+        .createHmac("sha256", [salt, password].join("/"))
+        .update(salt)
+        .digest("hex");
+};
+exports.authentication = authentication;
 //# sourceMappingURL=index.js.map
--- BE/src/helpers/index.ts
+++ BE/src/helpers/index.ts
@@ -1,4 +1,7 @@
 import bcryptjs from "bcryptjs";
+import crypto from "crypto";
+
+const SECRET = process.env.SECRET;
 
 export const hashPassword = async (password: string) => {
   return await bcryptjs.hash(password, 10);
@@ -6,4 +9,15 @@ export const hashPassword = async (password: string) => {
 
 export const comparePassword = async (password: string, hash: string) => {
   return await bcryptjs.compare(password, hash);
+};
+
+export const random = () => {
+  return crypto.randomBytes(128).toString("base64");
+};
+
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(salt)
+    .digest("hex");
 };
