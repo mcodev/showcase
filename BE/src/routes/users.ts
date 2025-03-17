203f4d9a165b8156ca803ee58a2525ac90d57700
mcodev <mcodevns@gmail.com>
2025-03-17 13:37:48 +0200

    update

--- BE/src/routes/users.ts
+++ BE/src/routes/users.ts
@@ -7,5 +7,10 @@ import { isAuthenticated, isOwner } from "../middlewares/auth";
 export default (router: express.Router) => {
   // TODO fix any
   router.get("/users", isAuthenticated as any, getAllUsers as any);
-  router.delete("/users/:id", isOwner as any, deleteUser as any);
+  router.delete(
+    "/users/:id",
+    isAuthenticated as any,
+    isOwner as any,
+    deleteUser as any
+  );
 };
