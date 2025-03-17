669b90adbf5db27ed4c9cde1024f02e583a3e960
mcodev <mcodevns@gmail.com>
2025-03-17 13:51:10 +0200

    update

--- BE/dist/middlewares/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// const errorHandler = (err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).json({ success: false, error: err.message });
-// };
-Object.defineProperty(exports, "__esModule", { value: true });
-// module.exports = errorHandler;
-//# sourceMappingURL=error.js.map
--- BE/dist/routes/authentication.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authentication_1 = require("../controllers/authentication");
-exports.default = (router) => {
-    //  TODO fix any
-    router.post("/auth/register", authentication_1.register);
-    router.post("/auth/login", authentication_1.login);
-};
-//# sourceMappingURL=authentication.js.map
