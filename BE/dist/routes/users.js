3a8241cab90acee08aa8dc408208bf43d9415d76
mcodev <mcodevns@gmail.com>
2025-03-17 13:38:02 +0200

    update

--- BE/dist/routes/users.js
+++ BE/dist/routes/users.js
@@ -5,6 +5,6 @@ const auth_1 = require("../middlewares/auth");
 exports.default = (router) => {
     // TODO fix any
     router.get("/users", auth_1.isAuthenticated, users_1.getAllUsers);
-    router.delete("/users/:id", auth_1.isOwner, users_1.deleteUser);
+    router.delete("/users/:id", auth_1.isAuthenticated, auth_1.isOwner, users_1.deleteUser);
 };
 //# sourceMappingURL=users.js.map
