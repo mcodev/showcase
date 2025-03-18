d91f39f266f6e169fc68acf205d2a066859ea21c
mcodev <mcodevns@gmail.com>
2025-03-18 20:32:50 +0200

    update

--- FE/web/features/auth/components/ForgotPassword.tsx
+++ FE/web/features/auth/components/ForgotPassword.tsx
@@ -1,35 +1,58 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Flex, Text, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { isEmail } from '@/common/validators';
 import { useAuthContext } from '../context/AuthSelectionProvider';
 
 const ForgotPassword = () => {
   const { changeSelectedComponent } = useAuthContext();
   const { t } = useTranslation();
 
+  const form = useForm({
+    mode: 'controlled',
+    initialValues: {
+      email: '',
+    },
+
+    validate: {
+      email: (value) => isEmail(value),
+    },
+  });
+
   return (
-    <Flex direction="column" mt="xl">
-      <TextInput label="Email" placeholder="email" labelProps={{ mb: '4px' }} />
+    <form onSubmit={form.onSubmit((values) => console.log('values', values))}>
+      <Flex direction="column" mt="xl">
+        <TextInput
+          label="Email"
+          placeholder="email"
+          labelProps={{ mb: '4px' }}
+          value={form.values.email}
+          onChange={(event) => form.setFieldValue('email', event.target.value)}
+          error={t(form.errors.email as string)}
+          required
+        />
 
-      <Button w="100%" mt="xl">
-        {t('send_reset_link')}
-      </Button>
+        <Button w="100%" mt="xl" type="submit">
+          {t('send_reset_link')}
+        </Button>
 
-      <Flex mt="xl" justify="center">
-        <Text c="dimmed" fw={400} mr="xs" size="sm">
-          {t('already_have_an_account')}
-        </Text>
+        <Flex mt="xl" justify="center">
+          <Text c="dimmed" fw={400} mr="xs" size="sm">
+            {t('already_have_an_account')}
+          </Text>
 
-        <Text
-          onClick={() => changeSelectedComponent('signIn')}
-          className="cursor_pointer hover_color"
-          fw={400}
-          size="sm"
-        >
-          {t('sign_in')}
-        </Text>
+          <Text
+            onClick={() => changeSelectedComponent('signIn')}
+            className="cursor_pointer hover_color"
+            fw={400}
+            size="sm"
+          >
+            {t('sign_in')}
+          </Text>
+        </Flex>
       </Flex>
-    </Flex>
+    </form>
   );
 };
 
