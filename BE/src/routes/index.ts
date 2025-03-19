101b2329703c1e15174573fb7b008787d6e9bf54
mcodev <mcodevns@gmail.com>
2025-03-19 22:34:22 +0200

    update

--- BE/src/routes/index.ts
+++ BE/src/routes/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import authentication from "./authentication";
-import users from "./users";
+// import users from "./users";
 
 const router = express.Router();
 
 export default (): express.Router => {
   authentication(router);
-  users(router);
+  // users(router);
 
   return router;
 };
