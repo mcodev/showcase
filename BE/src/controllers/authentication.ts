63735d3d0736fe7a415207da647b80227d2e0109
mcodev <mcodevns@gmail.com>
2025-03-19 13:52:02 +0200

    update

--- BE/src/controllers/authentication.ts
+++ BE/src/controllers/authentication.ts
@@ -11,11 +11,11 @@ export const login = async (
   try {
     const { email, password } = req.body;
 
-    // www.youtube.com/watch?v=XYjOteYbCMo
+    //www.youtube.com/watch?v=XYjOteYbCMo
 
     // TODO check fields with validators
 
-    https: if (!email || !password) {
+    if (!email || !password) {
       response({ res, statusCode: 400, route: ROUTES_NAMES.AUTH });
       return;
     }
