e1131c9fec619ba3c6ea0e75a16461f7136e1de2
mcodev <mcodevns@gmail.com>
2025-03-19 11:53:40 +0200

    update

---
+++ BE/src/helpers/validators.ts
@@ -0,0 +1,18 @@
+export const isValidName = (name: string) => {
+  const re = /^[A-Za-z]{3,}$/;
+
+  return re.test(name.trim());
+};
+
+export const isValidEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(email).toLowerCase());
+};
+
+export const isValidPassword = (password: string) => {
+  const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  return re.test(password);
+};
