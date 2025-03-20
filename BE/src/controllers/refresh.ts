df9323af88a376cc04e7565b8b94acc06a52dfda
mcodev <mcodevns@gmail.com>
2025-03-20 12:26:17 +0200

    update

---
+++ BE/src/controllers/refresh.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { response } from "../common";
+import { ROUTES_NAMES } from "../consts";
+import { generateAccessToken } from "../helpers/tokens";
+import { RefreshToken } from "../models/RefreshToken";
+
+export const refresh = async (
+  req: express.Request,
+  res: express.Response
+): Promise<void> => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    response({
+      res,
+      statusCode: 403,
+      route: ROUTES_NAMES.AUTH,
+      //   message: "No refresh token provided",
+    });
+    return;
+  }
+
+  // Check if the token exists in the DB
+  const storedToken = await RefreshToken.findOne({ token: refreshToken });
+  if (!storedToken) {
+    response({
+      res,
+      statusCode: 403,
+      route: ROUTES_NAMES.AUTH,
+      //   message: "Invalid refresh token",
+    });
+    return;
+  }
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET as string,
+    async (err: any, user: any) => {
+      if (err) {
+        response({
+          res,
+          statusCode: 403,
+          route: ROUTES_NAMES.AUTH,
+          //   message: "Invalid refresh token",
+        });
+        return;
+      }
+
+      const newAccessToken = generateAccessToken(user);
+      response({
+        res,
+        statusCode: 200,
+        route: ROUTES_NAMES.AUTH,
+        payload: { accessToken: newAccessToken },
+      }).end();
+    }
+  );
+};
