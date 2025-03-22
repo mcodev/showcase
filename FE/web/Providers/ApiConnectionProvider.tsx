e8ca4cccea864320a1d2fc3b4d73ac4114f08e10
mcodev <mcodevns@gmail.com>
2025-03-22 12:13:02 +0200

    update

--- FE/web/Providers/ApiConnectionProvider.tsx
+++ FE/web/Providers/ApiConnectionProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useCallback, useContext } from 'react';
 import secureLocalStorage from 'react-secure-storage';
 import { PROTECTED_ROUTES, SERVICE, SERVICES, ServicesSelectorType } from '@/services';
 import { GLOBAL_ERRORS } from '@/services/errors';
@@ -14,92 +14,79 @@ type RequestPropsType = {
   payload?: unknown;
 };
 
-type ServiceType = {
-  [key in keyof typeof SERVICES]: ServicesSelectorType;
-};
+const ApiConnectionContext = createContext<{
+  request: (props: RequestPropsType) => Promise<GlobalResponseDataType>;
+}>({
+  request: async () => ({ success: false, data: null, statusCode: 500 }),
+});
 
-export const ApiConnectionProvider = ({ children }: { children: React.ReactNode }) => {
+export const ApiConnectionProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { updateAccessToken, updateUser, accessToken } = useUserContext();
 
-  const handleAuthInitialization = async (data: LoginResponseDataType) => {
-    updateAccessToken(data.accessToken);
-    updateUser(data.user);
+  const handleAuthInitialization = useCallback(
+    async (data: LoginResponseDataType) => {
+      updateAccessToken(data.accessToken);
+      updateUser(data.user);
+      secureLocalStorage.setItem('rt', data.refreshToken);
+    },
+    [updateAccessToken, updateUser]
+  );
 
-    secureLocalStorage.setItem('rt', data.refreshToken);
-  };
+  const request = useCallback(
+    async ({ service, payload }: RequestPropsType): Promise<GlobalResponseDataType> => {
+      const { path, method, errors } = SERVICES[service];
 
-  const request = async ({
-    service,
-    payload,
-  }: RequestPropsType): Promise<GlobalResponseDataType> => {
-    const SELECTED_SERVICE = SERVICES[service];
+      try {
+        const response = await fetch(`${BASE_URL}/${path}`, {
+          method,
+          headers: {
+            'Content-Type': 'application/json',
+            ...(PROTECTED_ROUTES.includes(service) && accessToken
+              ? { Authorization: `Bearer ${accessToken}` }
+              : {}),
+          },
+          body: JSON.stringify(payload),
+          cache: 'no-cache',
+          referrerPolicy: 'no-referrer',
+        });
 
-    try {
-      const response = await fetch(`${BASE_URL}/${SELECTED_SERVICE.path}`, {
-        method: SELECTED_SERVICE.method,
-        headers: {
-          'Content-Type': 'application/json',
-          ...(PROTECTED_ROUTES.includes(service) ? { Authorization: `Bearer ${accessToken}` } : {}),
-        },
-        body: JSON.stringify(payload),
-        cache: 'no-cache',
-        referrerPolicy: 'no-referrer',
-      });
+        const data = await response.json();
+        const statusCode = response.status;
 
-      const data = await response.json();
+        if (!response.ok) {
+          if (data?.error === 'TOKEN_EXPIRED' && statusCode === 402) {
+            // TODO Handle token refresh logic here
+          }
 
-      const statusCode = response.status;
+          const errorMapping = Object.fromEntries(
+            errors.map(({ code, message }) => [code, message])
+          );
 
-      // Create an error mapping object
-      const errorMapping = Object.fromEntries(
-        SELECTED_SERVICE.errors.map(({ code, message }: { code: number; message: string }) => [
-          code,
-          message,
-        ])
-      );
-
-      if (!response.ok) {
-        const errorMessage = data?.error;
-
-        if (errorMessage === 'TOKEN_EXPIRED' && statusCode === 402) {
-          // get access token fro refresh an retry request
+          throw new Error(errorMapping[statusCode] || GLOBAL_ERRORS.UNEXPECTED_ERROR);
         }
 
-        throw new Error(errorMapping[statusCode] || GLOBAL_ERRORS.UNEXPECTED_ERROR);
-      }
+        if ([SERVICE.LOGIN_SERVICE, SERVICE.SIGN_UP_SERVICE].includes(service)) {
+          if (data.data?.accessToken && data.data?.refreshToken) {
+            await handleAuthInitialization(data.data);
 
-      if (service === SERVICE.LOGIN_SERVICE || service === SERVICE.SIGN_UP_SERVICE) {
-        if (data.data.accessToken && data.data.refreshToken) {
-          await handleAuthInitialization(data.data);
+            return { statusCode, success: data.success, data: null };
+          }
 
-          return { statusCode, success: data.success, data: null };
+          throw new Error('Unexpected authentication error');
         }
-        throw new Error('Unexpected error');
-      }
 
-      return { statusCode, success: data.success, data: data.data };
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(`Fetch function error: ${error.message}`);
-      } else {
-        throw error;
+        return { statusCode, success: data.success, data: data.data };
+      } catch (error) {
+        throw new Error(`Request error: ${(error as Error).message}`);
       }
-    }
-  };
+    },
+    [accessToken, handleAuthInitialization]
+  );
 
   return (
-    <ApiConnectionContext.Provider value={{ request, SERVICE }}>
-      {children}
-    </ApiConnectionContext.Provider>
+    <ApiConnectionContext.Provider value={{ request }}>{children}</ApiConnectionContext.Provider>
   );
 };
-
-const ApiConnectionContext = createContext({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  request: ({ service, payload }: RequestPropsType) =>
-    Promise.resolve({ success: false, data: null, statusCode: 500 }),
-  SERVICE: {} as ServiceType,
-});
 
 export const useApiConnection = () => useContext(ApiConnectionContext);
 
