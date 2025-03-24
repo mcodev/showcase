6ec54945e9fab5b1d97da0a55caedd575ce4d14b
mcodev <mcodevns@gmail.com>
2025-03-24 22:12:15 +0200

    update

--- BE/src/routes/refresh.ts
+++ BE/src/routes/refresh.ts
@@ -5,3 +5,44 @@ import { refresh } from "../controllers/refresh";
 export default (router: express.Router) => {
   router.post(`${ROUTES.REFRESH}`, refresh);
 };
+
+/**
+ * @swagger
+ * /refresh:
+ *   post:
+ *     summary: Refresh token
+ *     tags:
+ *       - Refresh
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *                 example: "your.jwt.token"
+ *     responses:
+ *       201:
+ *         description: Successfully refreshed token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 accessToken:
+ *                   type: string
+ *                   example: "your.jwt.token"
+ *                 refreshToken:
+ *                   type: string
+ *                   example: "your.jwt.token"
+ *       400:
+ *        description:  NO_REFRESH_TOKEN_PROVIDED
+ *       403:
+ *        description:  INVALID_REFRESH_TOKEN
+ *       404:
+ *        description:  REFRESH_TOKEN_NOT_FOUND
+ */
