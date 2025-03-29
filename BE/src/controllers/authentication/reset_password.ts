161a09ea950b8ae14682260440d40e8082d94d89
mcodev <mcodevns@gmail.com>
2025-03-29 09:12:42 +0200

    update

--- BE/src/controllers/authentication/reset_password.ts
+++ BE/src/controllers/authentication/reset_password.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { response } from "../../helpers/response";
 import { RESPONSE_MESSAGES } from "../../consts";
-import { isValidPassword } from "../../helpers/validators";
+import { isValidEmail, isValidPassword } from "../../helpers/validators";
+import { getUserByEmail, updateUser } from "../../models/Users";
+import jwt from "jsonwebtoken";
 
 const RESPONSE_MESSAGE = RESPONSE_MESSAGES.RESET_PASSWORD;
 
@@ -10,15 +12,40 @@ export const reset_password = async (
   res: express.Response
 ): Promise<void> => {
   try {
-    const { password, temporaryResetToken } = req.body;
-
-    // TODO check how its stored the reset token and get the user accordingly
+    const { password, temporaryResetToken, email } = req.body;
 
     if (!password) {
       response({
         res,
+        statusCode: 404,
+        message: RESPONSE_MESSAGE[404].MISSING_PASSWORD,
+      });
+      return;
+    }
+
+    if (!temporaryResetToken) {
+      response({
+        res,
+        statusCode: 404,
+        message: RESPONSE_MESSAGE[404].MISSING_TEMPORARY_RESET_TOKEN,
+      });
+      return;
+    }
+
+    if (!email) {
+      response({
+        res,
+        statusCode: 404,
+        message: RESPONSE_MESSAGE[404].MISSING_EMAIL,
+      });
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      response({
+        res,
         statusCode: 400,
-        message: RESPONSE_MESSAGE[400].MISSING_PASSWORD,
+        message: RESPONSE_MESSAGE[400].INVALID_EMAIL,
       });
       return;
     }
@@ -32,49 +59,33 @@ export const reset_password = async (
       return;
     }
 
-    if (!temporaryResetToken) {
-      response({
-        res,
-        statusCode: 400,
-        message: RESPONSE_MESSAGE[400].MISSING_TEMPORARY_RESET_TOKEN,
-      });
-      return;
-    }
+    const user = await getUserByEmail(email);
 
-    // const user = await getUserByResetCode(temporaryResetToken);
+    jwt.verify(
+      user.temporaryResetToken,
+      process.env.ACCESS_TOKEN_SECRET as string,
+      (err) => {
+        if (err) {
+          response({
+            res,
+            statusCode: 400,
+            message: RESPONSE_MESSAGE[400].INVALID_TEMP_RESET_TOKEN,
+          });
 
-    // // Find user with matching temporary reset token
-    // const user = await User.findOne({
-    //   $or: [{ email }, { phoneNumber }],
-    //   temporaryResetToken,
-    // });
+          return;
+        }
+      }
+    );
 
-    // if (!user) {
-    //   response({
-    //     res,
-    //     statusCode: 400,
-    //     // message: "Invalid reset request",
-    //     route: ROUTES_NAMES.AUTH,
-    //   });
-    //   return;
-    // }
+    await updateUser(String(user._id), {
+      password,
+      temporaryResetToken: null,
+    });
 
-    // // Hash new password
-    // const hashedPassword = await bcrypt.hash(password, 10);
-
-    // // Update password and clear reset-related fields
-    // user.password = hashedPassword;
-    // user.resetCode = null;
-    // user.resetCodeExpiry = null;
-    // user.temporaryResetToken = null;
-    // await user.save();
-
-    // response({
-    //   res,
-    //   statusCode: 200,
-    //   // message: "Password successfully reset",
-    //   route: ROUTES_NAMES.AUTH,
-    // });
+    response({
+      res,
+      statusCode: 200,
+    });
   } catch (error) {
     console.error("Reset Password Error:", error);
     response({
@@ -83,3 +94,40 @@ export const reset_password = async (
     });
   }
 };
+
+/**
+ * @swagger
+ * /auth/reset_password:
+ *   post:
+ *     summary: Reset password
+ *     description: Resets the user's password using a temporary reset token.
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               password:
+ *                 type: string
+ *                 example: "newPassword123"
+ *               temporaryResetToken:
+ *                 type: string
+ *                 example: "your-temporary-reset-token"
+ *               email:
+ *                 type: string
+ *                 example: "user@example.com"
+ *             required:
+ *               - password
+ *               - temporaryResetToken
+ *               - email
+ *     responses:
+ *       200:
+ *         description: Password reset successful
+ *       400:
+ *         description: INVALID_PASSWORD , INVALID_TEMP_RESET_TOKEN , INVALID_EMAIL
+ *       404:
+ *         description: MISSING_TEMPORARY_RESET_TOKEN , MISSING_EMAIL , MISSING_PASSWORD , RESET_CODE_NOT_FOUND , USER_NOT_FOUND
+ */
